603eee0b9d6333667ed2eae42e2b84e34909968f
classiccowl <khavari.7878@yahoo.com>
2025-03-20 09:45:50 +0000

    Updated repo

--- dsfut.net.py
+++ dsfut.net.py
@@ -1,52 +1,41 @@
-import time
-start_time = time.time()
-from datetime import datetime
-import calendar
-import requests
 import hashlib
 import time
-import os
-import winsound
-from colorama import init, Fore
-init()
-#os.system("CLS")
-n = 0
-fifa = "" # Enter fifa version
-console = "" # Enter platform
-partner_id = "" # Enter your partner id
-secret_key = "8dd2f198b0bf072ee93188cea3b5a77f" # Enter your secret key
-##### Buy Now Price will be ckecked!!!!
-min_buy_value = "" # Enter minimum price
-max_buy_value = "" # Enter maximum price
-take_after_value = "0"
-message = "popped"
-# d = datetime.utcnow()
-# unixtime = str(calendar.timegm(d.utctimetuple()))
-# print(unixtime)
-# 1634835812
-1634839922
-t = "1000000000"
-signature = partner_id + secret_key + t
-result = hashlib.md5(signature.encode()).hexdigest()
+from datetime import datetime
 
+import requests
+
+fifa = ""  # Enter fifa version
+console = ""  # Enter platform
+partner_id = ""  # Enter your partner id
+secret_key = ""  # Enter your secret key
+##### Buy Now Price will be ckecked!!!!
+min_buy_value = ""  # Enter minimum price
+max_buy_value = ""  # Enter maximum price
+take_after_value = "0"
+timestamp = str(int(datetime.now().timestamp()))
+pattern = partner_id + secret_key + timestamp
+signature = hashlib.md5(pattern.encode()).hexdigest()
+
+
+n = 0
 while True:
     n += 1
     now = datetime.now().time()
-    url = f"https://dsfut.net/api/{fifa}/{console}/{partner_id}/{t}/{result}?min_buy={min_buy_value}&max_buy={max_buy_value}"
+    url = f"https://dsfut.net/api/{fifa}/{console}/{partner_id}/{timestamp}/{signature}?min_buy={min_buy_value}&max_buy={max_buy_value}&take_after={take_after_value}"
     end_time = time.time()
-    print(start_time,end_time,end_time-start_time)
     r = requests.get(url)
-    
-    # print(r.json())
-    # print(type())
-    if message not in str(r.content):
-        print(Fore.RED + str(r.content) + "ReqNo."+ str(n), now)
-    else:
-        print(Fore.GREEN + str(r.content) + "ReqNo."+ str(n),now)
-        f = open ('C:\\report.txt' , 'a')
-        f.write(str(r.content) + "ReqNo."+ str(n) + '   ' + str(now))
-        f.writelines('\n')
-        f.close()
-        # #winsound.Beep(1000, 10000)
+    data = r.json()
+    error = data["error"]
+    if error == "throttle":
+        print(data["message"])
+        print(data, f"ReqNo.{n} {now}")
         break
-    time.sleep(0.334)
+    elif error == "":
+        print(data["message"])
+        print(data, f"ReqNo.{n} {now}")
+        break
+    else:
+        print(data["message"])
+        print(data, f"ReqNo.{n} {now}")
+
+    time.sleep(1)
